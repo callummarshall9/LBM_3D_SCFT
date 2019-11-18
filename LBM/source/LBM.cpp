3bca07ffd16e2dcd6fdc042fe507377c0969b437
callummarshall <callummarshall9@googlemail.com>
2019-11-18 04:22:02 +0000

    Updating previous commit

--- LBM/source/LBM.cpp
+++ LBM/source/LBM.cpp
@@ -2,16 +2,17 @@
 #include "vector3.cpp"
 #include <iomanip>
 #include <cmath>
-#include <assert.h>
+#include <vector>
+#include <random>
 
-LBM::LBM(int grid_size, std::string velocity_set, double m_c_s, double m_tau, std::string m_boundary_condition, double m_gamma_dot) : NX(grid_size),NY(grid_size),NZ(grid_size),
-        c_s(m_c_s), tau_LB(m_tau), boundary_condition(m_boundary_condition), gamma_dot(m_gamma_dot), velocity_set(velocity_set) {
+LBM::LBM(int grid_size, std::string velocity_set, double m_c_s, double m_tau, std::string m_boundary_condition, double m_gamma_dot, int m_runs) : NX(grid_size),NY(grid_size),NZ(grid_size),
+    c_s(m_c_s), tau_LB(m_tau), boundary_condition(m_boundary_condition), gamma_dot(m_gamma_dot), velocity_set(velocity_set), runs(m_runs) {
     set_velocity_set(velocity_set);
     initialise();
 }
 
-LBM::LBM(int nx, int ny, int nz, std::string velocity_set, double m_c_s, double m_tau, std::string m_boundary_condition, double m_gamma_dot) : NX(nx), NY(ny), NZ(nz),
-        c_s(m_c_s), tau_LB(m_tau), boundary_condition(m_boundary_condition), gamma_dot(m_gamma_dot), velocity_set(velocity_set) {
+LBM::LBM(int nx, int ny, int nz, std::string velocity_set, double m_c_s, double m_tau, std::string m_boundary_condition, double m_gamma_dot, int m_runs) : NX(nx), NY(ny), NZ(nz),
+    c_s(m_c_s), tau_LB(m_tau), boundary_condition(m_boundary_condition), gamma_dot(m_gamma_dot), velocity_set(velocity_set), runs(m_runs) {
     set_velocity_set(velocity_set);
     initialise();
 }
@@ -19,25 +20,28 @@ LBM::LBM(int nx, int ny, int nz, std::string velocity_set, double m_c_s, double 
 void LBM::initialise() {
     int box_flatten_length = NX * NY * NZ;
     int distributions_flatten_length = box_flatten_length * direction_size;
-    density_field = new double[box_flatten_length];
+    density_field = new double[box_flatten_length * (runs+1)];
     velocity_field = new vector3<double>[box_flatten_length];
     particle_distributions_star = new double[distributions_flatten_length];
     particle_distributions = new double[distributions_flatten_length];
     field_plus = new double[box_flatten_length];
     field_minus = new double[box_flatten_length];
     for(int i = 0; i < NX * NY * NZ; i++) {
+        field_plus[i] = 0.0;
+        field_minus[i] = 0.0;
         density_field[i] = 1.0;
     }
     for(int x = 0; x < NX; x++) {
         for(int y = 0; y < NY; y++) {
             for(int z = 0; z < NZ; z++) {
                 for(int i = 0; i < direction_size; i++) {
-                    particle_distributions_star[scalar_index(x,y,z,i)] = weights[i] * density_field[scalar_index(x,y,z)];
-                    particle_distributions[scalar_index(x,y,z,i)] = weights[i] * density_field[scalar_index(x,y,z)];
+                    particle_distributions_star[scalar_index(x,y,z,i)] = weights[i] * density_field[scalar_index(x,y,z,time)];
+                    particle_distributions[scalar_index(x,y,z,i)] = weights[i] * density_field[scalar_index(x,y,z,time)];
                 }
             }
         }
     }
+
 }
 
 double LBM::calculate_feq(int x, int y, int z, int i) {
@@ -47,7 +51,7 @@ double LBM::calculate_feq(int x, int y, int z, int i) {
     //double feq = weights[i] * density_field[scalar_index(x,y,z)] * (1.0 + dot_product / (c_s * c_s) + dot_product * dot_product / (2 * c_s * c_s * c_s * c_s) - velocity_field[scalar_index(x,y,z)].norm_square() / (2 * c_s * c_s));
     //Equation (21) from Lattice Boltzmann Method for multiscale self-consistent field theory
     //simulations of block copolymers by Hsieh Chen, YongJoo Kim and Alfredo Alexander-Katz.
-    double feq = weights[i] * density_field[scalar_index(x,y,z)];
+    double feq = weights[i] * density_field[scalar_index(x,y,z,time)];
     return feq;
 }
 
@@ -55,9 +59,9 @@ double LBM::calculate_feq(int x, int y, int z, int i, double u_le_x) {
     double dot_product = (velocity_field[scalar_index(x,y,z)].x + u_le_x) * directions[i].x + velocity_field[scalar_index(x,y,z)].y * directions[i].y +
                          velocity_field[scalar_index(x,y,z)].z * directions[i].z;
     double norm_square = (velocity_field[scalar_index(x,y,z)].x + u_le_x) * (velocity_field[scalar_index(x,y,z)].x + u_le_x) + velocity_field[scalar_index(x,y,z)].y * directions[i].y +
-                            velocity_field[scalar_index(x,y,z)].z * directions[i].z;
+                         velocity_field[scalar_index(x,y,z)].z * directions[i].z;
     //Equation 3.4 with c_s^2 = 1/3
-    double feq = weights[i] * density_field[scalar_index(x,y,z)] * (1.0 + dot_product / (c_s * c_s) + dot_product * dot_product / (2 * c_s * c_s * c_s * c_s) - norm_square / (2 * c_s * c_s));
+    double feq = weights[i] * density_field[scalar_index(x,y,z,time)] * (1.0 + dot_product / (c_s * c_s) + dot_product * dot_product / (2 * c_s * c_s * c_s * c_s) - norm_square / (2 * c_s * c_s));
     return feq;
 }
 
@@ -69,19 +73,26 @@ void LBM::set_velocity_set(std::string velocity_set) {
     } else if(velocity_set == "D3Q27") {
         direction_size = 27;
         directions = new vector3<int>[27]{{0,0,0}, {1,0,0},{-1,0,0},
-        {0,1,0},{0,-1,0},{0,0,1},{0,0,-1},{1,1,0},{-1,-1,0},{1,0,1},
-        {-1,0,-1},{0,1,1},{0,-1,-1},{1,-1,0},{-1,1,0},{1,0,-1},{-1,0,1},
-        {0,1,-1},{0,-1,1},{1,1,1},{-1,-1,-1},{1,1,-1},{-1,-1,1},{1,-1,1},
-        {-1,1,-1},{-1,1,1},{1,-1,-1}};
+                                          {0,1,0},{0,-1,0},{0,0,1},{0,0,-1},{1,1,0},{-1,-1,0},{1,0,1},
+                                          {-1,0,-1},{0,1,1},{0,-1,-1},{1,-1,0},{-1,1,0},{1,0,-1},{-1,0,1},
+                                          {0,1,-1},{0,-1,1},{1,1,1},{-1,-1,-1},{1,1,-1},{-1,-1,1},{1,-1,1},
+                                          {-1,1,-1},{-1,1,1},{1,-1,-1}};
         weights = new double[27] {8.0/27.0,2.0/27.0,2.0/27.0,2.0/27.0,
-            2.0/27.0,2.0/27.0,2.0/27.0, 1.0/54.0,1.0/54.0,1.0/54.0
-        ,1.0/54.0,1.0/54.0,1.0/54.0,1.0/54.0,1.0/54.0,1.0/54.0,
-        1.0/54.0,1.0/54.0,1.0/54.0, 1.0/216.0, 1.0/216.0, 1.0/216.0, 1.0/216.0
-        ,1.0/216.0, 1.0/216.0, 1.0/216.0, 1.0/216.0};
+                                  2.0/27.0,2.0/27.0,2.0/27.0, 1.0/54.0,1.0/54.0,1.0/54.0
+                ,1.0/54.0,1.0/54.0,1.0/54.0,1.0/54.0,1.0/54.0,1.0/54.0,
+                                  1.0/54.0,1.0/54.0,1.0/54.0, 1.0/216.0, 1.0/216.0, 1.0/216.0, 1.0/216.0
+                ,1.0/216.0, 1.0/216.0, 1.0/216.0, 1.0/216.0};
     } else if(velocity_set == "D2Q9") {
         direction_size = 9;
         directions = new vector3<int>[9]{{1,0,0},{0,1,0},{-1,0,0},{0,-1,0},{1,1,0},{-1,1,0},{-1,-1,0},{1,-1,0},{0,0,0}};
         weights = new double[9]{1.0/9.0,1.0/9.0,1.0/9.0,1.0/9.0,1.0/36.0,1.0/36.0,1.0/36.0,1.0/36.0,4.0/9.0};
+    } else if(velocity_set == "D3Q7") {
+        direction_size = 7;
+        directions = new vector3<int>[7]{ {0,0,0},{1,0,0},{-1,0,0},{0,1,0},{0,-1,0},{0,0,1},{0,0,-1}};
+        weights = new double[7] { 1.0/4.0, 1.0/8.0, 1.0/8.0, 1.0/8.0, 1.0/8.0, 1.0/8.0, 1.0/8.0 };
+        if(round(c_s * c_s * 100.0) / 100.0 == 0.33) {
+            std::cout << "Warning: The D3Q7 velocity set uses c_s=1/sqrt(2)" << '\n';
+        }
     }
     lookup_reverse();
 
@@ -136,7 +147,7 @@ void LBM::set_velocity(int x_field, int y_field, int z_field, double u_x, double
 }
 
 void LBM::set_density(int x_field, int y_field, int z_field, double density) {
-    density_field[scalar_index(x_field,y_field,z_field)] = density;
+    density_field[scalar_index(x_field,y_field,z_field,time)] = density;
 }
 
 
@@ -155,7 +166,7 @@ void LBM::compute_density_momentum_moment() {
                     u.y += particle_distributions[scalar_index(x,y,z,i)] * directions[i].y;
                     u.z += particle_distributions[scalar_index(x,y,z,i)] * directions[i].z;
                 }
-                density_field[scalar_index(x,y,z)] = new_density;
+                density_field[scalar_index(x,y,z, time)] = new_density;
                 velocity_field[scalar_index(x,y,z)].x = u.x / new_density;
                 velocity_field[scalar_index(x,y,z)].y = u.y / new_density;
                 velocity_field[scalar_index(x,y,z)].z = u.z / new_density;
@@ -203,46 +214,46 @@ void LBM::stream() {
                             particle_distributions[scalar_index(x,y,z,i)] = particle_distributions_star[scalar_index(xmd,ymd,zmd,i)];
                         }
                     } else if(boundary_condition == "lees_edwards") {
-                            double d_x = gamma_dot * (double)NY * (double)time;
-                            int d_x_I = d_x;
-                            double d_x_R = d_x - d_x_I;
-                            d_x_I = d_x_I % NX;
-                            double s_1 = d_x_R;
-                            double s_2 = 1.0 - d_x_R;
-                            //s_1=0;s_2=1;
-                            int ymd = (NY + y - directions[i].y) % NY;
-                            int zmd = (NZ + z - directions[i].z) % NZ;
-                            if(y==0 && directions[i].y == 1) {
-                                //Bottom Wall.
-                                //Equation (17) from Less-Edwards boundary conditions for lattice Boltzmann suspension simulations
-                                //by Eric Lorenz and Alfons G. Hoekstra
-                                    int x_pos;
-                                    x_pos = (x+d_x_I + 2 * NX - directions[i].x) % NX;
-                                    int x_shifted;
-                                    x_shifted = (x+d_x_I + 2 * NX +1 - directions[i].x) % NX;
-                                    double galilean_transformation_pos = particle_distributions_star[scalar_index(x_pos,ymd,zmd,i)] + calculate_feq(x_pos,ymd,zmd,i,-1 * gamma_dot * (double)NY) - calculate_feq(x_pos,ymd,zmd,i,0);
-                                    double galilean_transformation_shift = particle_distributions_star[scalar_index(x_shifted,ymd,zmd,i)] + calculate_feq(x_shifted,ymd,zmd,i,-1 * gamma_dot * (double)NY) - calculate_feq(x_shifted,ymd,zmd,i,0);
-                                    //Equation (18) from same paper.
-                                    particle_distributions[scalar_index(x,y,z,i)]= s_1 * galilean_transformation_shift +
-                                                                                   s_2 * galilean_transformation_pos;
-                                } else if(y==NY-1 && directions[i].y == -1) {
-                                    //Equation (17) from Less-Edwards boundary conditions for lattice Boltzmann suspension simulations
-                                    //by Eric Lorenz and Alfons G. Hoekstra
-                                    //Top Wall.
-                                    int x_pos;
-                                    x_pos = (x-d_x_I + 2 * NX - directions[i].x) % NX;
-                                    int x_shifted;
-                                    x_shifted = (x-d_x_I + 2 * NX -1 - directions[i].x) % NX;
-                                    double galilean_transformation_pos = particle_distributions_star[scalar_index(x_pos,ymd,zmd,i)] + calculate_feq(x_pos,ymd,zmd,i,1 * gamma_dot * (double)NY) - calculate_feq(x_pos,ymd,zmd,i,0);
-                                    double galilean_transformation_shift = particle_distributions_star[scalar_index(x_shifted,ymd,zmd,i)] + calculate_feq(x_shifted,ymd,zmd,i,1 * gamma_dot * (double)NY) - calculate_feq(x_shifted,ymd,zmd,i,0);
-                                    //Equation (18) from same paper.
-                                    particle_distributions[scalar_index(x,y,z,i)]= s_1 * galilean_transformation_shift +
-                                                                                   s_2 * galilean_transformation_pos;
-                                } else {
-                                    //Interior points. (Enforce periodicity along x axis);.
-                                    int xmd = (NX + x - directions[i].x) % NX;
-                                    particle_distributions[scalar_index(x,y,z,i)] = particle_distributions_star[scalar_index(xmd,ymd,zmd,i)];
-                                }
+                        double d_x = gamma_dot * (double)NY * (double)time;
+                        int d_x_I = d_x;
+                        double d_x_R = d_x - d_x_I;
+                        d_x_I = d_x_I % NX;
+                        double s_1 = d_x_R;
+                        double s_2 = 1.0 - d_x_R;
+                        //s_1=0;s_2=1;
+                        int ymd = (NY + y - directions[i].y) % NY;
+                        int zmd = (NZ + z - directions[i].z) % NZ;
+                        if(y==0 && directions[i].y == 1) {
+                            //Bottom Wall.
+                            //Equation (17) from Less-Edwards boundary conditions for lattice Boltzmann suspension simulations
+                            //by Eric Lorenz and Alfons G. Hoekstra
+                            int x_pos;
+                            x_pos = (x+d_x_I + 2 * NX - directions[i].x) % NX;
+                            int x_shifted;
+                            x_shifted = (x+d_x_I + 2 * NX +1 - directions[i].x) % NX;
+                            double galilean_transformation_pos = particle_distributions_star[scalar_index(x_pos,ymd,zmd,i)] + calculate_feq(x_pos,ymd,zmd,i,-1 * gamma_dot * (double)NY) - calculate_feq(x_pos,ymd,zmd,i,0);
+                            double galilean_transformation_shift = particle_distributions_star[scalar_index(x_shifted,ymd,zmd,i)] + calculate_feq(x_shifted,ymd,zmd,i,-1 * gamma_dot * (double)NY) - calculate_feq(x_shifted,ymd,zmd,i,0);
+                            //Equation (18) from same paper.
+                            particle_distributions[scalar_index(x,y,z,i)]= s_1 * galilean_transformation_shift +
+                                                                           s_2 * galilean_transformation_pos;
+                        } else if(y==NY-1 && directions[i].y == -1) {
+                            //Equation (17) from Less-Edwards boundary conditions for lattice Boltzmann suspension simulations
+                            //by Eric Lorenz and Alfons G. Hoekstra
+                            //Top Wall.
+                            int x_pos;
+                            x_pos = (x-d_x_I + 2 * NX - directions[i].x) % NX;
+                            int x_shifted;
+                            x_shifted = (x-d_x_I + 2 * NX -1 - directions[i].x) % NX;
+                            double galilean_transformation_pos = particle_distributions_star[scalar_index(x_pos,ymd,zmd,i)] + calculate_feq(x_pos,ymd,zmd,i,1 * gamma_dot * (double)NY) - calculate_feq(x_pos,ymd,zmd,i,0);
+                            double galilean_transformation_shift = particle_distributions_star[scalar_index(x_shifted,ymd,zmd,i)] + calculate_feq(x_shifted,ymd,zmd,i,1 * gamma_dot * (double)NY) - calculate_feq(x_shifted,ymd,zmd,i,0);
+                            //Equation (18) from same paper.
+                            particle_distributions[scalar_index(x,y,z,i)]= s_1 * galilean_transformation_shift +
+                                                                           s_2 * galilean_transformation_pos;
+                        } else {
+                            //Interior points. (Enforce periodicity along x axis);.
+                            int xmd = (NX + x - directions[i].x) % NX;
+                            particle_distributions[scalar_index(x,y,z,i)] = particle_distributions_star[scalar_index(xmd,ymd,zmd,i)];
+                        }
 
 
                     }
@@ -277,7 +288,8 @@ void LBM::set_field_paramaeters(double N, double Length_Rg, std::string m_field_
     const double L = (double)NX * delta_x;
     R_g = L / box_length_rg;
     const double b = sqrt(6) / sqrt(chain_length) * R_g;
-    tau_LB = (3.0 * b * b / 6.0 + 0.5);
+    //tau_LB = (3.0 * b * b / 6.0 + 0.5);
+    tau_LB = (b * b) / (6.0 * c_s * c_s) + 0.5;
     field_type = m_field_type;
 }
 
@@ -306,7 +318,7 @@ void LBM::collision() {//Performs the collision step.
             for(int z = 0; z < NZ; z++) {
                 for(int i = 0; i < direction_size; i++) {
 
-                    double R = -field(x,y,z) * density_field[scalar_index(x,y,z)];
+                    double R = -field(x,y,z) * density_field[scalar_index(x,y,z,time)];
                     double feq = calculate_feq(x,y,z,i);
                     //Equation 20
                     particle_distributions_star[scalar_index(x,y,z,i)] = particle_distributions[scalar_index(x,y,z,i)] +  tauinv * (feq - particle_distributions[scalar_index(x,y,z,i)])  + weights[i] * R;
@@ -325,7 +337,26 @@ double gauss_noise(double x) {
 }
 
 void LBM::perform_field_calculations() {
-    double C = 1.0;
+    const double C = 20000.0;
+    const double flory_higgs = 0.5;
+    const double gamma = 1.0;
+    double Q = 0.0;
+    double sum = 0;
+    double h = 1.0;
+
+    //Obtain the zero frequency component as Q(w) = a_0(N), according to FFTW
+    //the zero frequency is at index 0.
+    sum = sum / (NX * NY * NZ);
+    std::default_random_engine generator;
+    std::normal_distribution<double> dist(0, 1);
+    for(int x = 0; x < NX; x++) {
+        for(int y = 0; y < NY; y++) {
+            for(int z = 0; z < NZ; z++) {
+
+            }
+        }
+    }
+    /*double C = 1.0;
     double T = 1.0;
     double florry_higgs = 0.5;
     double N = 1000;//Total number of statistical segments.
@@ -338,7 +369,7 @@ void LBM::perform_field_calculations() {
                 field_minus[scalar_index(x,y,z)] = field_minus[scalar_index(x,y,z)] - T * C * (-1.0 * density_minus + 2.0 / (florry_higgs * N) * field_minus[scalar_index(x,y,z)]) + gauss_noise(time);
             }
         }
-    }
+    }*/
 }
 
 int LBM::get_time() {return time;}
@@ -368,7 +399,7 @@ void LBM::output_f_array(double* f_array, int z_index) {
 
 void LBM::output_lbm_data(std::string filename, bool header, bool output_indices) {
     std::ofstream output_stream;
-  output_stream.open (filename, std::ofstream::out | std::ofstream::app);
+    output_stream.open (filename, std::ofstream::out | std::ofstream::app);
     if(header) {
         if(output_indices) {
             output_stream << "x,y,z,";
@@ -383,12 +414,38 @@ void LBM::output_lbm_data(std::string filename, bool header, bool output_indices
                 if(output_indices) {
                     output_stream << x << "," << y << "," << z << ",";
                 }
-                output_stream << density_field[this->scalar_index(x,y,z)] << "," <<
-                    velocity_field[this->scalar_index(x,y,z)].x << "," <<
-                    velocity_field[this->scalar_index(x,y,z)].y << "," <<
-                    velocity_field[this->scalar_index(x,y,z)].z << "," << field(x, a, b) << '\n';
+                output_stream << density_field[scalar_index(x,y,z,time)] << "," <<
+                              velocity_field[scalar_index(x,y,z)].x << "," <<
+                              velocity_field[scalar_index(x,y,z)].y << "," <<
+                              velocity_field[scalar_index(x,y,z)].z << "," << field(x, a, b) << '\n';
             }
         }
     }
     output_stream.close();
+}
+
+
+
+double LBM::find_max() {
+    double max = 0;
+    for(int x = 0; x < NX; x++) {
+        for(int y = 0; y < NY; y++ ) {
+            for(int z = 0; z < NZ; z++) {
+                if(density_field[scalar_index(x,y,z,time)] > max) {
+                    max = density_field[scalar_index(x,y,z,time)];
+                }
+            }
+        }
+    }
+    return max;
+}
+
+double LBM::find_min() {
+    double min = density_field[scalar_index(0,0,0, time)];
+    for(int x = 0; x < NX; x++) {
+        if(density_field[scalar_index(x,0,0, time)] < min) {
+            min = density_field[scalar_index(x,0,0, time)];
+        }
+    }
+    return min;
 }
